97fb5658aedf61e46f9e08fe3ae9d75fd51b71d4
fliegerin <grigoryeva.lena@gmail.com>
2024-05-30 12:09:23 +0500

    Game_v3 function update

--- Guess_number_game/game_v3.py
+++ Guess_number_game/game_v3.py
@@ -1,9 +1,9 @@
 
 import numpy as np
 
+
 def game_core_v3(number: int = 1) -> int:
-    """Устанавливаем любое random число, потом уменьшаем или 
-    увеличиваем его на 2 в зависимости от того, четные числа или нечетные.
+    """Устанавливаем число - середину диапазона, затем в зависимости от того, больше или меньше загаданное число, сдвигаем диапазон и снова предполагаем его середину - до тех пор, пока диапазон не сузится до загаданного числа.
        Функция принимает загаданное число и возвращает число попыток
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
@@ -13,26 +13,23 @@ def game_core_v3(number: int = 1) -> int:
     """
     # Ваш код начинается здесь
     count = 0
-    predict = np.random.randint(1, 101)
+    minimum = 1
+    maximum = 100
+    predict = (minimum+maximum) // 2
+    
+    if predict == number:
+        count = 1
 
     while number != predict:
         count += 1
-        if number % 2 == 0:
-            if predict % 2 == 0:
-                if number > predict:
-                    predict += 2
-                elif number < predict:
-                    predict -= 2
-            else:
-                predict += 1
-        else: 
-            if predict % 2 != 0:
-                if number > predict:
-                    predict += 2
-                elif number < predict:
-                    predict -= 2
-            else:
-                predict += 1
+        if predict < number:
+            minimum = predict + 1
+            predict = (minimum + maximum) // 2 
+
+        elif predict > number:
+            maximum = predict - 1
+            predict = (minimum + maximum) // 2
+
 
     # Ваш код заканчивается здесь
 
